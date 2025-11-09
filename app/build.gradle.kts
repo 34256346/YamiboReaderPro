22666003a885492d2006591463000b416679870b
FengYunJie <1727588371@qq.com>
2025-11-09 19:53:14 +0800

    v1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.shirakawatyu.yamibo.novel"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "v1.2.1"
+        versionCode = 7
+        versionName = "v1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
